a917351abd0afbfd7bc2e1afe02f3693f0c3abc1
ashlin07 <furtadoashlin07@gmail.com>
2024-03-27 18:05:57 +0530

    initial commit

---
+++ src/main/java/com/ashlin/RateFlix/RateFlixApplication.java
@@ -0,0 +1,13 @@
+package com.ashlin.RateFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashlin/RateFlix/RateFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashlin.RateFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
